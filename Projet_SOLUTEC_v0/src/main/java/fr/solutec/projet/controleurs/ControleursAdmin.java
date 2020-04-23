a8410998cd198b4cfd6c6800c26250940efa0417
psaintec <49141629+psaintec@users.noreply.github.com>
2020-04-23 17:21:36 +0200

    controleur ajout et delete

--- Projet_SOLUTEC_v0/src/main/java/fr/solutec/projet/controleurs/ControleursAdmin.java
+++ Projet_SOLUTEC_v0/src/main/java/fr/solutec/projet/controleurs/ControleursAdmin.java
@@ -1,10 +1,8 @@
 package fr.solutec.projet.controleurs;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,84 +11,88 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import fr.solutec.projet.modeles.Admin;
-import fr.solutec.projet.modeles.Categorie;
 import fr.solutec.projet.modeles.Produit;
 import fr.solutec.projet.modeles.ProduitRepository;
 
 @Controller
-@RequestMapping(path="/admin")
+@RequestMapping(path="/")
 public class ControleursAdmin {
 
 	@Autowired 
 	  private ProduitRepository produitRepository;
-	  	
-	
-			
-	// Appel de la page d'accueil admin :
-	@GetMapping
+	  			
+	// Appel de la page d'accueil admin via l'URL :
+	@GetMapping(path="/admin")
 	public String getAdmin(@ModelAttribute("admin") Admin admin) {
 		return "accueilAdmin";
 	}
 	
-	// Accès à la page gestionAdmin :
-	@PostMapping
-	public String getGestionAdmin(HttpServletRequest request, @ModelAttribute("produit") Produit produit, @ModelAttribute("admin") Admin admin, Map<String, Object> model) {
+	// Connexion à la page gestionAdmin (login : admin et mdp : SOLUTEC) :
+	@PostMapping(path="/admin")
+	public String getGestionAdmin(HttpServletRequest request, @ModelAttribute("produit") Produit produit, 
+			@ModelAttribute("admin") Admin admin, Map<String, Object> model) {
 		if (request.getParameter("login").equals("admin") &&
-				request.getParameter("password").equals("SOLUTEC")) 
-		{
-			
-			
-			if(produit.getCategorie_id()!=null && produit.getMarque()!=null && produit.getPrix()!=null && produit.getPhoto()!=null && produit.getProduit_name()!=null)
-			{
-				produitRepository.save(produit);
-				return "gestionAdmin";
-			}
-			
-			model.put("gestionAdmin", produitRepository.findAll());
+				request.getParameter("password").equals("SOLUTEC")) {			
+			model.put("produits", produitRepository.findAll());
 			return "gestionAdmin";
-			
-			
-		} 
-		
-	else
+		} else
 			return "accueilAdmin";
 	} 
 	
-	
-	
-//	@PostMapping
-//	public String postAjouterProduit(@Valid @ModelAttribute("produit") Produit produit, BindingResult result )
-//	{
-//		
-//		if(produit.getCategorie_id()!=null && produit.getMarque()!=null && produit.getPrix()!=null && produit.getPhoto()!=null && produit.getProduit_name()!=null)
-//		{
-//			produitRepository.save(produit);
-//			System.out.println("ajout effectué");
-//			return "gestionAdmin";
-//			
-//			
-//		}	
-//		
-//		System.out.println("echec ajout");
-//		return "gestionAdmin";
-		
-//		 if(result.hasErrors()) {
-//			  return "gestionAdmin";
-//		  }
-		 
-//		  produitRepository.save(produit);
-//		  return "gestionAdmin";
-		
+	// Tri des données :
+//	@GetMapping(path="/admin")
+//	public String sortProduit(HttpServletRequest request, Map<String, Object> model) {
+//		if(request.getParameter("critere").equals("produit_id"))
+//			model.put("produits", produitRepository.selectProduitParId());
+//		if(request.getParameter("critere").equals("produit_name"))
+//			model.put("produits", produitRepository.selectProduitParNom());
 //	}
 	
+	  
+	// Ajout d'un produit dans la base de données :
+	@PostMapping(path="/adminAjouter")
+	public String addProduitAdmin (@ModelAttribute("produit") Produit produit, HttpServletRequest request,
+			BindingResult result, Map<String, Object> model) {
+		if(produit.getCategorie_id()!=null &&
+				produit.getMarque()!=null &&
+				produit.getPrix()!=null &&
+				produit.getPhoto()!=null &&
+				produit.getProduit_name()!=null) {
+			produit.setProduit_name(request.getParameter("produit_name"));
+			produit.setCategorie_id(Integer.parseInt(request.getParameter("categorie_id")));
+			produit.setMarque(request.getParameter("marque"));
+			produit.setPrix(request.getParameter("prix"));
+			produit.setPhoto(request.getParameter("photo")); 
+//			produit.setClasse_Energetique_Id(request.getParameter("classe_energetique_id"));
+			produitRepository.save(produit);
+			model.put("produits", produitRepository.findAll());
+			return "gestionAdmin";
+		} else {
+			System.out.println("echec ajout");
+			return "gestionAdmin";
+		}	
+	}
+
+	// Modification d'un produit dans la base de données :
+//	@PostMapping(path="/adminModifier")
+//	public String setProduitAdmin (@ModelAttribute("produit") Produit produit, HttpServletRequest request,
+//			BindingResult result, Map<String, Object> model) {
+//		produit.setProduit_name(request.getParameter("produit_name"));
+//		produit.setCategorie_id(Integer.parseInt(request.getParameter("categorie_id")));
+//		produit.setMarque(request.getParameter("marque"));
+//		produit.setPrix(request.getParameter("prix"));
+//		produit.setPhoto(request.getParameter("photo")); 
+////			produit.setClasse_Energetique_Id(request.getParameter("classe_energetique_id"));
+//		produitRepository.save(produit);
+//		model.put("produits", produitRepository.findAll());
+//		return "gestionAdmin";
+//	}
 	
 	// Suppression d'un produit :
-		@GetMapping(path="/supprimer")
-		public String supprimerProduit(HttpServletRequest request, Map<String, Object> model) {
+		@GetMapping(path="/adminSupprimer")
+		public String deleteProduitAdmin(HttpServletRequest request, Map<String, Object> model) {
 			if(request.getParameter("produit_id")!=null) {
 				produitRepository.deleteById(Integer.parseInt(request.getParameter("produit_id")));
 			}
@@ -99,97 +101,5 @@ public class ControleursAdmin {
 		}
 	
 		
-		
-	@PostMapping(path="/ajouter")
-	public String ajoutProduitAdmin (@ModelAttribute("produit") Produit produit, HttpServletRequest request, BindingResult result, Map<String, Object> model)
-	{
-		  produit.setProduit_name(request.getParameter("produit_name"));
-		  produit.setCategorie_id(request.getParameter("categorie_id"));
-		  produit.setMarque(request.getParameter("marque"));
-		  produit.setPrix(request.getParameter("prix"));
-		  produit.setPhoto(request.getParameter("photo")); 
-		  produit.setClasse_Energetique_Id(request.getParameter("classe_energetique_id"));
-		  produitRepository.save(produit);
-		  System.out.println("ajout effectué");
-		  return "gestionAdmin";	
-		
-//		if(produit.getCategorie_id()!=null && produit.getMarque()!=null && produit.getPrix()!=null && produit.getPhoto()!=null && produit.getProduit_name()!=null)
-//		{
-//			produitRepository.save(produit);
-//			System.out.println("ajout effectué");
-//			
-//			
-//		}	
-//		
-//		else
-//		{
-//			System.out.println("echec ajout");
-//			return "gestionAdmin";
-//		}	
-	}
-	
-	
-	
-//	@GetMapping(path="/ajouter")
-//	public String getAjoutProduit(@Valid @ModelAttribute("produit") Produit produit, BindingResult result) {
-//		
-//		return "ajoutProduit";
-//	}
-//	
-	
-//	@PostMapping(path="/ajouter")
-//	public String postAjouterProduit(@Valid @ModelAttribute("produit") Produit produit, BindingResult result)
-//	{
-//		if(produit.getCategorie_id()!=null && produit.getMarque()!=null && produit.getPrix()!=null && produit.getPhoto()!=null && produit.getProduit_name()!=null)
-//		{
-//			produitRepository.save(produit);
-//			System.out.println("ajout ok");
-//			return "gestionAdmin";
-//		}
-//		
-//		else 
-//		{
-//			System.out.println("ajout echec");
-//			return "gestionAdmin";
-//		}
-//		
-//	}
-	
-	
-//	// Ajout d'un produit :
-//	@GetMapping(path="/gestionProduits")
-//	public String getProduit(@ModelAttribute("produit") Produit produit) {
-//		return "gestionAdmin";
-//	}
-	
-	// Affichage des produits sans filtre:
-	@GetMapping(path="/produits")
-	public @ResponseBody Iterable<Produit> getAllUsers() {
-		return produitRepository.findAll();  
-	}
-	
-	
-//	// Affichage de tous les produits :
-//	@GetMapping(path="/")
-//	public String getProduits(Map<String, Object> model) {
-//		model.put("gestionAdmin", produitRepository.findAll());
-//		return "gestionAdmin";
-//		}
-	
-//	// Création des catégories d'electroménager :
-//    @RequestMapping(value="/gestionAdmin")
-//    private ModelAndView selectTag() {
-//        ModelAndView mav = new ModelAndView("categorieForm");
-//         
-//        Map<Integer, String > categories = new HashMap<Integer, String>();
-//        categories.put(1, "Réfrigérateur");
-//        categories.put(2, "Lave-vaisselle");
-//        categories.put(3, "Four");
-//        categories.put(4, "Plaque de cuisson");
-//         
-//        mav.addObject("categoriesMap", categories);
-//        mav.addObject("electromenager", new Categorie());
-//         
-//        return mav;
-//    }
+
 }
