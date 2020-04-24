6c62d9e243e663adc903042593fa3241af3f836c
psaintec <49141629+psaintec@users.noreply.github.com>
2020-04-24 12:57:11 +0200

    Mise à jour controleur admin

--- Projet_SOLUTEC_v0/src/main/java/fr/solutec/projet/controleurs/ControleursAdmin.java
+++ Projet_SOLUTEC_v0/src/main/java/fr/solutec/projet/controleurs/ControleursAdmin.java
@@ -30,61 +30,78 @@ public class ControleursAdmin {
 	}
 
 	// Connexion à la page gestionAdmin (login : admin et mdp : SOLUTEC) :
-	@PostMapping(path = "/admin")
-	public String getGestionAdmin(HttpServletRequest request, @ModelAttribute("produit") Produit produit,
-			@ModelAttribute("admin") Admin admin, Map<String, Object> model) {
-		if (request.getParameter("login").equals("admin") && request.getParameter("password").equals("SOLUTEC")) {
-			model.put("produits", produitRepository.findAll());
-			return "gestionAdmin";
-		} else
-			return "accueilAdmin";
-	}
+		@PostMapping(path="/admin")
+		public String getGestionAdmin(HttpServletRequest request, @ModelAttribute("produit") Produit produit, 
+				@ModelAttribute("admin") Admin admin, Map<String, Object> model) {
+			if (request.getParameter("login").equals("admin") &&
+					request.getParameter("password").equals("SOLUTEC")) {
+//				Admin administrateur = new Admin(Integer.parseInt(request.getParameter("idAdmin")), 
+//						request.getParameter("login").trim(), 
+//						request.getParameter("password").trim());
+//				HttpSession session = request.getSession();
+//			    session.setAttribute("admin", administrateur);
+//			    out.println("Vous êtes connecté avec le nom de compte admin");
+				model.put("produits", produitRepository.findAll());
+				return "gestionAdmin";
+			} else
+				return "accueilAdmin";
+		} 
+		
+		
+//	// Vérifier les paramètres de connexion :
+//    // Si les identifiants sont bons :
+//    if(request.getParameter("username").trim().contains("user") && 
+//            request.getParameter("password").trim().contains("user")) {
+//                User user = new User(
+//                request.getParameter("username").trim(),
+//                request.getParameter("password").trim()
+//            );
+//            HttpSession session = request.getSession();
+//            session.setAttribute("user", user);
+//            out.println("<div class = 'succes'>Vous êtes connecté avec le nom de compte "
+//                    + request.getParameter("username") + ".</div>");
+//    }
+//    // Si les identifiants sont mauvais :
+//    else {
+//            out.println("<div class = 'echec'>Paramètres de connexions incorrects !<br><br></div>");
+//            request.getRequestDispatcher("/WEB-INF/Include/login_form.html").include(request, response);
+//    }
+
 
 	// Tri des données :
-//	@GetMapping(path="/admin")
-//	public String sortProduit(HttpServletRequest request, Map<String, Object> model) {
-//		if(request.getParameter("critere").equals("produit_id"))
-//			model.put("produits", produitRepository.selectProduitParId());
-//		if(request.getParameter("critere").equals("produit_name"))
-//			model.put("produits", produitRepository.selectProduitParNom());
-//	}
+//		@GetMapping(path="/adminTri")
+//		public String sortProduit(HttpServletRequest request, Map<String, Object> model) {
+//			if(request.getParameter("critere").equals("produit_idAsc"))
+//				model.put("produits", produitRepository.selectProduitParIdAsc());
+//			if(request.getParameter("critere").equals("produit_idDesc"))
+//				model.put("produits", produitRepository.selectProduitParIdDesc());
+//			if(request.getParameter("critere").equals("produit_nameAsc"))
+//				model.put("produits", produitRepository.selectProduitParNomAsc());
+//			if(request.getParameter("critere").equals("produit_nameDesc"))
+//				model.put("produits", produitRepository.selectProduitParNomDesc());
+//			return "gestionAdmin";
+//		}
 
-	// Ajout d'un produit dans la base de données :
-	@PostMapping(path = "/adminAjouter")
-	public String addProduitAdmin(@ModelAttribute("produit") Produit produit, HttpServletRequest request,
-			BindingResult result, Map<String, Object> model) {
 
-		if(produit.getCategorie_id()!=null &&
-				produit.getMarque()!=null &&
-				produit.getPrix()!=null &&
-				produit.getPhoto()!=null &&
-				produit.getProduit_name()!=null) {
-//			produit.setProduit_name(request.getParameter("produit_name"));
-//			produit.setCategorie_id(Integer.parseInt(request.getParameter("categorie_id")));
-//			produit.setMarque(request.getParameter("marque"));
-//			produit.setPrix(request.getParameter("prix"));
-//			produit.setPhoto(request.getParameter("photo")); 
-//			produit.setClasse_Energetique_Id(request.getParameter("classe_energetique_id"));
-
-		if (produit.getCategorie_id() != null && produit.getMarque() != null && produit.getPrix() != null
-				&& produit.getPhoto() != null && produit.getProduit_name() != null) {
-			produit.setProduit_name(request.getParameter("produit_name"));
-			produit.setCategorie_id(Integer.parseInt(request.getParameter("categorie_id")));
-			produit.setMarque(request.getParameter("marque"));
-			produit.setPrix(Integer.parseInt(request.getParameter("prix")));
-			produit.setPhoto(request.getParameter("photo"));
-			produit.setClasse_energetique_id(Integer.parseInt(request.getParameter("classe_energetique_id")));
-
-			produitRepository.save(produit);
-			model.put("produits", produitRepository.findAll());
-			return "gestionAdmin";
-		} else {
-			System.out.println("echec ajout");
-			return "gestionAdmin";
+		// Ajout d'un produit dans la base de données :
+		@PostMapping(path="/adminAjouter")
+		public String addProduitAdmin (@ModelAttribute("produit") Produit produit, HttpServletRequest request,
+				BindingResult result, Map<String, Object> model) {
+			if(produit.getCategorie_id()!=null &&
+					produit.getMarque()!=null &&
+					produit.getPrix()!=null &&
+					produit.getPhoto()!=null &&
+					produit.getProduit_name()!=null) {
+				produitRepository.save(produit);
+				model.put("produits", produitRepository.findAll());
+				return "gestionAdmin";
+			} else {
+				System.out.println("echec ajout");
+				return "gestionAdmin";
+			}	
 		}
-	}
-		return"gestionAdmin";
-	}
+
+
 	
 	
 	
